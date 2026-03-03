c04b4d42f4d06c93d4664076a0a734949ef0b7a8
Quang Thinh Le <quangthinh2924@gmail.com>
2026-03-03 16:04:42 +0700

    feat: add WebSocket and network_info_plus dependencies for LAN foundation
    - Add web_socket_channel ^3.0.1 (cross-platform WebSocket client)
    - Add network_info_plus ^6.1.0 (get local IP on Android)
    - Add Android network permissions: INTERNET, ACCESS_NETWORK_STATE,
      ACCESS_WIFI_STATE, CHANGE_WIFI_MULTICAST_STATE

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import awesome_notifications
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import network_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
